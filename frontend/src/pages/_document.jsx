835bf363b0744324bc6bdb0bb2883c3f02045c93
Pablito471 <pablohernanbrito@gmail.com>
2026-02-08 03:00:43 -0300

    correccion de bugs

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon.svg" />
+        <meta name="theme-color" content="#3b82f6" />
+      </Head>
       <body>
         <Main />
         <NextScript />
